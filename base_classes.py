c0bee432531a9f1274605405f8728e4efe48ff41
plaf2000 <plaf2000@gmail.com>
2023-10-24 02:05:28 +0200

    Working on fast ffmpeg splits.

--- base_classes.py
+++ base_classes.py
@@ -5,15 +5,32 @@ from math import ceil
 from typing import Generator
 import random
 import numpy as np
+import tempfile
+import subprocess
+from datetime import datetime, timedelta
+import re
 
 import os
 import csv
 
+dateel_lengths = {
+    "%Y": 4,
+    "%y": 2,
+    "%m": 2,
+    "%d": 2,
+    "%j": 3,
+    "%H": 2,
+    "%I": 2,
+    "%p": 2,
+    "%M": 2,
+    "%S": 2,
+    "%f": 6,
+}
 
 class TimeUnit(float):
-    def __init__(self, s: float = 0, ms: float = None):
+    def __init__(self, s: float | str = 0, ms: float | str | None = None):
         if ms is not None:
-            s = s / 1000
+            s = float(ms) / 1000
         self = float(s)
 
     def __add__(self, other):
@@ -49,6 +66,20 @@ class Detection:
         self.tend = TimeUnit(float(tend))
         self.label = label    
         
+    @property
+    def dur(self) -> TimeUnit:
+        return self.tend - self.tstart
+    
+    def centered_pad(self, pad: TimeUnit):
+        return Detection(self.tstart - pad, self.tend + pad, self.label)
+
+    def centered_pad_to(self, duration: TimeUnit):
+        return self.centered_pad((duration - self.dur)/2)
+
+    def birdnet_pad(self):
+        return self.centered_pad_to(BIRDNET_AUDIO_DURATION)
+    
+    
 
 class DurDetection(Detection):
     def __init__(self, tstart, dur, label, audio_file):
@@ -59,9 +90,33 @@ class AudioFile:
     exported_intervals: list[list]
     exported_mask: np.ndarray
     
-    def __init__(self, path):
+    def __init__(self, path: str):
         self.path = path
         self.basename = os.path.basename(path).split(".")[0]
+        self.date_time = None
+    
+    def set_date(self, date_format:str = "%Y%m%d_%H%M%S"):
+        self.date_format = date_format
+        fre = date_format
+        for k, v in dateel_lengths.items():
+            fre = fre.replace(k,r"\d{" + str(v) + r"}")
+        m = re.search(fre, self.basename)
+        if m:
+            self.prefix, self.suffix = re.split(fre, self.basename)
+            self.date_time = datetime.strptime(m.group(0), date_format)
+
+    def detection_path(self, base_path: str, detection: Detection, audio_format = "flac") -> str:
+        out_path = os.path.join(base_path, detection.label)
+        os.makedirs(out_path)
+    
+        if self.date_time is not None:
+            date = self.date_time + timedelta(seconds = detection.dur.s)
+            name = f"{self.prefix}{date.strftime(self.date_format)}{self.suffix}.{audio_format}"
+        else:
+            name = f"{self.basename}.{audio_format}"
+        
+        return os.path.join(out_path, name)
+
 
     def load(self):
         if self.audio_segment is None:
@@ -151,8 +206,107 @@ class AudioFile:
                 tstart = TimeUnit(s=ts) + i * BIRDNET_AUDIO_DURATION
                 tend = tstart + BIRDNET_AUDIO_DURATION
                 self.export_segment(tstart, tend, dir_path, audio_format, mark=False, **kwargs)
-            
+    
+    def export_all_birdnet(self, base_path: str, detections: list[Detection], audio_format: str = "flac", overlap = 0):
+        detections = sorted([d.birdnet_pad() for d in detections], key=lambda det: det.tstart)
+        for det in detections:
+            self.exported_mask[int(det.tstart.s):int(ceil(det.tend.s))] = True
+        not_exp = ~self.exported_mask
+        diff = np.diff(not_exp.astype(np.int8), prepend=0, append=0)
 
+        tstamps = np.flatnonzero(np.abs(diff) == 1)
+        tstamps_cmd = np.array2string(tstamps, prefix="", suffix="", separator=",")
+
+
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            tmppath = lambda fname: os.path.join(tmpdirname, fname)
+            listpath = tmppath(f"{self.basename}.csv")
+
+            subprocess.run(
+                args=[
+                    "ffmpeg",
+                    "-i",
+                    self.path,
+                    "-f",
+                    "segment",
+                    "-segment_times",
+                    tstamps_cmd,
+                    "-segment_list",                    
+                    listpath,
+                    "-segment_list_entry_prefix",
+                    tmpdirname,
+                    tmppath(tmpdirname)
+                ]
+            )
+
+            det = detections.pop(0)
+            
+            with open(listpath) as fp:
+                csv_reader = csv.reader(fp)
+                for row in csv_reader:
+                    tstart_f = TimeUnit(s=row[1])
+                    tend_f = TimeUnit(s=row[2])
+
+                    if (tstart_f-tend_f).s < 3:
+                        continue
+
+                    fpath = row[0]
+
+                    noise = True
+                    while det.tstart < tend_f:
+                        if det.dur.s <= BIRDNET_AUDIO_DURATION:
+                            ss = det.tstart - tstart_f
+                            to = det.tend - tstart_f
+                            subprocess.run(
+                                args = [
+                                    "ffmpeg",
+                                    "-i",
+                                    fpath,
+                                    "-ss",
+                                    str(ss.s),
+                                    "-to",
+                                    str(to.s),
+                                    self.detection_path(base_path, det, audio_format)
+                                ]
+                            )
+                        
+                        else:
+                            tt = det.tstart - tstart_f
+                            start = True
+                            while start or tt < tend_f:
+                                tend = tstart + BIRDNET_AUDIO_DURATION
+                                tstart = tend - overlap
+                                start = False
+                        noise = False
+                        
+
+
+                    if noise:
+                        basename_noise_split = os.path.basename(fpath).split(".")
+                        basepath_noise = os.path.join(base_path, "Noise")
+                        os.makedirs(basepath_noise, exist_ok=True)
+                        basepath_out = os.path.join(basepath_noise, ".".join(basename_noise_split[:-1]))
+
+                        subprocess.run(
+                            args=[
+                                "ffmpeg",
+                                "-i",
+                                fpath,
+                                "-f",
+                                "segment",
+                                "-segment_time",
+                                str(BIRDNET_AUDIO_DURATION.s),
+                                "-segment_list",                    
+                                listpath,
+                                "-segment_list_entry_prefix",
+                                tmpdirname,
+                                f"{basepath_out}\%04d.{audio_format}"
+                            ]
+                        )
+
+                    
+
+                    
 
 @dataclass
 class Column:
