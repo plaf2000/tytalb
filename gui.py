e4f84e5687db7350dc03a1c046b0c52b7f8d545f
plaf2000 <paolo.laffranchi@vogelwarte.ch>
2023-12-05 11:25:04 +0100

    Chunk overlap.

--- gui.py
+++ gui.py
@@ -1,5 +1,7 @@
+import os
 from tkinter import ttk, filedialog
 from parsers import available_parsers
+from variables import BIRDNET_AUDIO_DURATION
 import tkinter as tk
 
 def input_command(var: tk.StringVar, filedialog_asker):
@@ -68,7 +70,14 @@ audio_files_frame = tk.Frame(extract_tab)
 audio_dir_name = tk.StringVar(audio_files_frame)
 audio_dir_label = ttk.Label(audio_files_frame, text = "Audio files directory:")
 audio_dir_chosen = ttk.Entry(audio_files_frame, textvariable = audio_dir_name)
-audio_dir_button = ttk.Button(audio_files_frame, text="Choose directory", command = input_command(audio_dir_name, filedialog.askdirectory))
+
+def audio_dir_command():
+    input_command(audio_dir_name, filedialog.askdirectory)()
+    labels_path = os.path.join(audio_dir_name.get(), "labels.json")
+    if os.path.isfile(labels_path):
+        label_settings.set(labels_path)
+
+audio_dir_button = ttk.Button(audio_files_frame, text="Select", command = audio_dir_command)
 
 audio_dir_label.pack(side="left")
 audio_dir_chosen.pack(side="left")
@@ -88,10 +97,50 @@ out_dir_button.pack(side="left")
 out_dir_frame.pack(pady=(10,0))
 
 # Labels settings
+label_settings_frame = tk.Frame(extract_tab)
+label_settings = tk.StringVar(label_settings_frame)
+label_settings_label = ttk.Label(label_settings_frame, text = "Label settings file:")
+label_settings_chosen = ttk.Entry(label_settings_frame, textvariable = label_settings)
+label_settings_button = ttk.Button(label_settings_frame, text="Select", command = input_command(label_settings, filedialog.askopenfilename))
+
+label_settings_label.pack(side="left")
+label_settings_chosen.pack(side="left")
+label_settings_button.pack(side="left")
+label_settings_frame.pack(pady=(10,0))
+
+#Chunk overlap
+def validate_overlap(P, V) -> bool:
+    if P=="":
+        if V=="focusout":
+            P = 0.0
+            chunk_overlap.set(P)
+        return True
+    try:
+        P = float(P)
+    except ValueError:
+        return False
+    return  0 <= P < BIRDNET_AUDIO_DURATION
 
 
+chunk_overlap_frame = tk.Frame(extract_tab)
+vcmd = chunk_overlap_frame.register(validate_overlap)
+chunk_overlap = tk.DoubleVar(chunk_overlap_frame)
+chunk_overlap_entry = ttk.Entry(chunk_overlap_frame, validate="all", textvariable=chunk_overlap, validatecommand=(vcmd, "%P", "%V"))
+
+chunk_overlap_frame.pack()
+chunk_overlap_entry.pack()
+
+# Date format (input audio files)
 
 
+# Late start
 
+# Early stop
+
+# Include path
+
+# Stats only
+
+# Noise export ratio
 
 window.mainloop()
